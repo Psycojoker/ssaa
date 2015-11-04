df21d4276f63d974609680999465fef6c8517f4b
Laurent Peuch <cortex@worlddomination.be>
2015-11-04 02:34:41 +0100

    [fix] credit is 6 chars

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,6 @@ class Movement(models.Model):
     date = models.DateField()
     title = models.CharField(max_length=500)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
-    kind = models.CharField(choices=(('credit', 'Crédit'), ('debit', 'Débit')), max_length=5)
+    kind = models.CharField(choices=(('credit', 'Crédit'), ('debit', 'Débit')), max_length=6)
 
     comment = models.TextField(null=True, blank=True)
